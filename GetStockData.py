55f0b4a98e9b9175696a41ab6d149a4ac8a88c0a
sajafleming <sajafleming@gmail.com>
2014-04-14 20:07:42 -0500

    Wrote code to get data into sqlite data base

--- GetStockData.py
+++ GetStockData.py
@@ -1,1 +1,19 @@
-# This file downloads stock market data and writes it to a MySQL table
+# This file downloads stock market data and writes it to a SQLite table
+import sqlite3
+import os #operating system 
+DATA_PATH = 'C:\Users\Dave\Documents\StockData'
+conn = sqlite3.connect('stockanalysis.db')
+c = conn.cursor()
+for fname in os.listdir(DATA_PATH):
+	print(fname)
+	f = open(os.path.join(DATA_PATH, fname))
+
+	for line in f.readlines():
+		[date, stock_open, high, low, stock_close, volume, adj_close] = line.rstrip().split(',')
+		c.execute('INSERT INTO raw_data VALUES (?,?,?)', (os.path.splitext(fname)[0], date, float(adj_close))) 
+		#tuple, replace question marks by those 3 values
+	
+	f.close() 
+
+conn.commit()
+conn.close()
